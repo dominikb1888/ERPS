06ff1a02399703c740a9fe54acd7b08821ddcc1b
Dominik Böhler <289612+dominikb1888@users.noreply.github.com>
2021-07-05 16:16:50 +0200

    final 21S

--- Slack/app.py
+++ Slack/app.py
@@ -1,10 +1,5 @@
-from flask import Flask, request
-from slack_bolt.adapter.flask import SlackRequestHandler
-from slack_bolt import App
-import logging
 import os
-
-logging.basicConfig(level=logging.DEBUG)
+from slack_bolt import App
 
 
 app = App(
@@ -12,82 +7,35 @@ app = App(
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
-flask_app = Flask(__name__)
-handler = SlackRequestHandler(app)
+# Listens to incoming messages that contain "hello"
 
 
-@app.middleware  # or app.use(log_request)
-def log_request(logger, body, next):
-    logger.debug(body)
-    return next()
-
-
-@app.event("app_mention")
-def event_test(body, say, logger):
-    logger.info(body)
-    say("What's up?")
-
-
-@app.event("message")
-def handle_message():
-    pass
-
-
-@app.route("/slack/events", methods=["POST"])
-def slack_events():
-    return handler.handle(request)
-
-
-@app.event("app_home_opened")
-def update_home_tab(client, event, logger):
-    try:
-        # views.publish is the method that your app uses to push a view to the Home tab
-        client.views_publish(
-            # the user that opened your app's app home
-            user_id=event["user"],
-            # the view object that appears in the app home
-            view={
-                "type": "home",
-                "callback_id": "home_view",
-
-                # body of the view
-                "blocks": [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "*Welcome to your _App's Home_* :tada:"
-                        }
-                    },
-                    {
-                        "type": "divider"
-                    },
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-                        }
-                    },
-                    {
-                        "type": "actions",
-                        "elements": [
-                            {
-                                "type": "button",
-                                "text": {
-                                    "type": "plain_text",
-                                    "text": "Click me!"
-                                }
-                            }
-                        ]
-                    }
-                ]
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Click Me"},
+                    "action_id": "button_click"
+                }
             }
-        )
+        ],
+        text=f"Hey there <@{message['user']}>!"
+    )
 
-    except Exception as e:
-        logger.error(f"Error publishing home tab: {e}")
+
+@app.action("button_click")
+def action_button_click(body, ack, say):
+    # Acknowledge the action
+    ack()
+    say(f"<@{body['user']['id']}> clicked the button")
 
 
+# Start your app
 if __name__ == "__main__":
-    app.start(3000)  # POST http://localhost:3000/slack/events
+    app.start(port=int(os.environ.get("PORT", 5000)))
